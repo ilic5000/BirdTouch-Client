8357014e65bf2893ae133f5963cd6c53c46468d1
ilic5000 <ilic1993@gmail.com>
2016-07-02 14:30:44 +0200

    Postavljeni elementi prvog activitiyja

--- BirdTouch/Resources/Resource.Designer.cs
+++ BirdTouch/Resources/Resource.Designer.cs
@@ -47,6 +47,9 @@ namespace BirdTouch
 			// aapt resource value: 0x7f020000
 			public const int Icon = 2130837504;
 			
+			// aapt resource value: 0x7f020001
+			public const int Logo = 2130837505;
+			
 			static Drawable()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
@@ -60,8 +63,17 @@ namespace BirdTouch
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f050002
+			public const int btnRegister = 2131034114;
+			
+			// aapt resource value: 0x7f050001
+			public const int btnSignIn = 2131034113;
+			
+			// aapt resource value: 0x7f050003
+			public const int progressBar1 = 2131034115;
+			
 			// aapt resource value: 0x7f050000
-			public const int MyButton = 2131034112;
+			public const int txtLogo = 2131034112;
 			
 			static Id()
 			{
