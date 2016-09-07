c166922e1d666715e43f86792a5c91ffd0851254
ilic5000 <ilic1993@gmail.com>
2016-09-07 20:09:33 +0200

    Izbacen Server iz Solutiona

--- BirdTouch Server/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BirdTouch_Server.WebApiApplication" Language="C#" %>
--- BirdTouch Server/Models/SimpleUser.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BirdTouch_Server.Models
-{
-    public class SimpleUser
-    {
-        public int Id { get; set; }
-        public string Username { get; set; }
-        public string Password { get; set; }
-    }
-}
